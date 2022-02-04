502621c17290805ba63caf099ce3f9019bd4de0a
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-02-04 16:53:39 +0300

    Update PrimeVue 2.8.0 and PrimeIcons 5.0.0

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,38 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <Toast />
-      
-      <form @submit.prevent="greet">
-          <InputText type="text" v-model="text"/>
-          <Button type="submit" label="Submit"/>
-      </form>
-     
+    <div class="container">
+        <div>
+            <Logo />
+            <Toast />
+            
+            <form @submit.prevent="greet">
+                <InputText type="text" v-model="text"/>
+                <Button type="submit" label="Submit"/>
+            </form>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      text: null
+    data() {
+        return {
+        text: null
+        }
+    },
+    methods: {
+        greet() {
+            this.$toast.add({severity: 'info', summary: 'Hello '  + this.text});
+        }
     }
-  },
-  methods: {
-			greet() {
-				this.$toast.add({severity: 'info', summary: 'Hello '  + this.text});
-			}
-  }
 }
 </script>
 
 <style>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 </style>
